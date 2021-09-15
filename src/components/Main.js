eba70786b65c503615142f12b05ed829f3be9355
Anthony V. Naut <83789702+anthonynaut@users.noreply.github.com>
2021-09-15 02:34:57 -0400

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import pic01 from '../images/pic01.jpg'
-import pic02 from '../images/pic02.jpg'
+import pic02 from '../images/pic02.mp4'
 import pic03 from '../images/pic03.jpg'
 
 class Main extends React.Component {
@@ -33,24 +33,33 @@ class Main extends React.Component {
             <img src={pic01} alt="" />
           </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
+            My interests in Technology have always been there ever since I was a child (video games!).
+            But I really got interested in my High School years wondering how the things around me worked.
+            So I joined the Robotics team! I did great and got all A's but, it wasn't my gig. I became increasingly
+            interested with the computer softwares that I would use to build 3d models of the robots that I want to bring to life (I also took Greentech
+            classes and got Outstanding grade in it too! We used a similar 3d modeling software where one can create their 
+            very own city! So cool!). I had little knowlegde on how things operated a computer. Therefore leading me to want to expand my knowlegde in it without having
+            to take out my music classes and other electives. Sadly, I graduated without having the chance to get computer science classes on my transcript.
+            Right out of High School I wasn't sure what I was going to do. Until my older brother reached out to me 
+            about this really cool website called code cademy where I can learn to code <b>FOR FREE. </b>
+             So, I gave it a shot, and fell in love with the craft instantly. I started to code for fun and do tutorials online to see what I can manage. 
+            Soon enough, it started to become me coding my own projects and I decided that I Wanted to level up in life.
+            Now knowing that I was set on this path I had to figure out how I was going to do this.
+            I started doing extensive researching and ended up with Three options: Self Learning, College, and Coding Bootcamp.
+            I tried the self learning route and it is tough because there is no guide to aid you in any direction.
+            Then I considered College for a while but became dissapointed with numbers that these colleges were asking for.
+            I knew I couldn't afford that, and needed to focus my attention on only coding. So I did my research for coding bootcamps
+            and found Coding Temple! I immediately told my older brother about them and he was on board so much so that He offered to pay for the bootcamp in Full if I promised to pay him when I'm on my feet! I went all in 
+            and went through hell and back to graduate the program (it was so much harder than I thought. Coding Bootcamps is like 4 years of college into 3 months. At least in my case it was 3months in length. Crazy!).
+            I came out the other side with skills I could have only dreamt of just a few months ago! I really learned a lot about myself
+            and was shocked that I was able to absorb so much so quickly then.
+            I'm now actively seeking to contribute to something bigger than myself with me and my skillset to offer.
+
+
+
+            
+            
+        
           </p>
           {close}
         </article>
@@ -66,20 +75,17 @@ class Main extends React.Component {
           <span className="image main">
             <img src={pic02} alt="" />
           </span>
+          
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/JprOmndVx8M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+         <div className="play-video">
+</div>
+
           <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
+          Here one of my bigger projects that i'm proud of. It's a balisong tutorial site with a store(under developement).
+          for everything used here is the link to my <a href='https://github.com/anthonynaut/V2-Capstone'>github</a>
           </p>
           <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
+      
           </p>
           {close}
         </article>
@@ -96,13 +102,9 @@ class Main extends React.Component {
             <img src={pic03} alt="" />
           </span>
           <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
+          Ambitious, career-focused jobseeker, anxious to obtain an entry-level software engineering position to help launch career while achieving company goals.
+          Enthusiastic programmer eager to contribute to team success through hard work, attention to detail and excellent organizational skills.
+          Motivated to learn, grow and excel in Software engineering.
           </p>
           {close}
         </article>
